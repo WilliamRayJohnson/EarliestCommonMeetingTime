c9ecf269397110e2ed8165c5822119bd9a55d3c0
William Ray Johnson <wrj6844@truman.edu>
2018-02-13 16:03:03 -0600

    Added logic necessary for thread creation and communication

--- earliestMeeting.c
+++ earliestMeeting.c
@@ -14,7 +14,7 @@
 
 typedef struct Input {
     double search_time;
-    FILE *time_sets;
+    char *time_sets;
 } th_arg_val;
 
 typedef struct Output {
@@ -27,34 +27,63 @@ void *find_common_time(void *arg);
 int main(int argc, char *argv[]) {
     if(argc==2) {
         int i;
-        FILE *fps[MAX_THREADS + 1];
+        FILE *mainFp;
         pthread_t *tid[MAX_THREADS];
         for (i = 0; i < MAX_THREADS; i++)
             tid[i] = (pthread_t *) malloc(sizeof(pthread_t));
-        
-        fps[0] = fopen(argv[1], "r");
+
+        mainFp = fopen(argv[1], "r");
         int setCount;
-        fscanf(fps[0], "%d", &setCount);
-        fgetc(fps[0]);
+        fscanf(mainFp, "%d", &setCount);
+        fgetc(mainFp);
 
         double d;
         char c;
+        int currTh = 0;
         do {
-            fscanf(fps[0], "%lf", &d);
-            printf("%lf\n", d);
-            fscanf(fps[0], "%c", &c);
+            fscanf(mainFp, "%lf", &d);
+            th_arg_val *argVal = (th_arg_val *) malloc(sizeof(th_arg_val));
+            argVal->search_time = d;
+            argVal->time_sets = argv[1];
+            if (pthread_create (tid[currTh], NULL, find_common_time, 
+                        (void *) argVal)) {
+                fprintf(stderr, "Error creating thread %d", currTh);
+                exit(THREAD_CREATION_FAILED);
+            } 
+            currTh++;
+            fscanf(mainFp, "%c", &c);
         } while(c != '\n');
 
-        fclose(fps[0]);
+        void **retVal = (void **) malloc(sizeof(void *));
+        for (i = 0; i < currTh; i++) {
+            if (pthread_join (*tid[i], retVal)) {
+                fprintf(stderr, "Error joining thread %d", i);
+                exit(THREAD_JOIN_FAILED);
+            }
+            else {
+                th_ret_val *result = (th_ret_val *) *retVal;
+                printf("The time, %lf, was found to be: i\n", result->common_time);
+
+            }            
+        }
+
+        fclose(mainFp);
     }
     else {
         printf("Please provide a text file's name as an argument\n");
     }
-    
+
     exit(0);
 }
 
 void *find_common_time(void *arg) {
+    double time;
+    char *file;
+    th_arg_val *input = (th_arg_val *) arg;
+    printf("Thread with %lf sees file %s\n", input->search_time, input->time_sets);
 
-    return (NULL);
+    th_ret_val *retVal = (th_ret_val *) malloc(sizeof(th_ret_val));
+    retVal->common_time = input->search_time;
+
+    return ((void *) retVal);
 }
