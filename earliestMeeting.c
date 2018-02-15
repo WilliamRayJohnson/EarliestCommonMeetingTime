d6a0c9c96231cc9eafb319a7fc8b4133e5a8c393
William Ray Johnson <wrj6844@truman.edu>
2018-02-14 19:33:13 -0600

    Added checks for empty file and an empty first set

--- earliestMeeting.c
+++ earliestMeeting.c
@@ -29,48 +29,55 @@ void *find_common_time(void *arg);
 int main(int argc, char *argv[]) {
     if(argc>=2) {
         int i;
+        double earliestCommonTime = DBL_MAX;
         FILE *mainFp;
         pthread_t *tid[MAX_THREADS];
 
         mainFp = fopen(argv[1], "r");
-        int setCount;
-        fscanf(mainFp, "%d", &setCount);
-        fgetc(mainFp);
+        fseek(mainFp, 0, SEEK_END);
+        if (ftell(mainFp) != 0) {
+            fseek(mainFp, 0, SEEK_SET);
+            int setCount;
+            fscanf(mainFp, "%d", &setCount);
+            fgetc(mainFp);
 
-        double d;
-        char c;
-        int currTh = 0;
-        double earliestCommonTime = DBL_MAX;
-        do {
-            fscanf(mainFp, "%lf", &d);
-            th_arg_val *argVal = (th_arg_val *) malloc(sizeof(th_arg_val));
-            argVal->search_time = d;
-            argVal->time_sets = argv[1];
-            argVal->thread_id = currTh;
-            tid[currTh] = (pthread_t *) malloc(sizeof(pthread_t));
-            if (pthread_create (tid[currTh], NULL, find_common_time, 
-                        (void *) argVal)) {
-                fprintf(stderr, "Error creating thread %d", currTh);
-                exit(THREAD_CREATION_FAILED);
-            } 
-            currTh++;
-            fscanf(mainFp, "%c", &c);
-        } while(c != '\n');
-        fclose(mainFp);
+            char c;
+            c = fgetc(mainFp);
+            if (c != '\n') {
+                ungetc(c, mainFp);
+                double d;
+                int currTh = 0;
+                do {
+                    fscanf(mainFp, "%lf", &d);
+                    th_arg_val *argVal = (th_arg_val *) malloc(sizeof(th_arg_val));
+                    argVal->search_time = d;
+                    argVal->time_sets = argv[1];
+                    argVal->thread_id = currTh;
+                    tid[currTh] = (pthread_t *) malloc(sizeof(pthread_t));
+                    if (pthread_create (tid[currTh], NULL, find_common_time, 
+                                (void *) argVal)) {
+                        fprintf(stderr, "Error creating thread %d", currTh);
+                        exit(THREAD_CREATION_FAILED);
+                    } 
+                    currTh++;
+                    fscanf(mainFp, "%c", &c);
+                } while(c != '\n');
 
-        void **retVal = (void **) malloc(sizeof(void *));
-        for (i = 0; i < currTh; i++) {
-            if (pthread_join (*tid[i], retVal)) {
-                fprintf(stderr, "Error joining thread %d", i);
-                exit(THREAD_JOIN_FAILED);
+                void **retVal = (void **) malloc(sizeof(void *));
+                for (i = 0; i < currTh; i++) {
+                    if (pthread_join (*tid[i], retVal)) {
+                        fprintf(stderr, "Error joining thread %d", i);
+                        exit(THREAD_JOIN_FAILED);
+                    }
+                    else {
+                        th_ret_val *result = (th_ret_val *) *retVal;
+                        if (result->val_found && result->common_time <= earliestCommonTime)
+                            earliestCommonTime = result->common_time;
+                    }            
+                }
             }
-            else {
-                th_ret_val *result = (th_ret_val *) *retVal;
-                if (result->val_found && result->common_time <= earliestCommonTime)
-                    earliestCommonTime = result->common_time;
-            }            
         }
-
+        fclose(mainFp);
         if (earliestCommonTime != DBL_MAX) 
             printf("%lf\n", earliestCommonTime);
         else 
